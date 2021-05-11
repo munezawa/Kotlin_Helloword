e2d55110ac68a373d805c247ab1a8457ea040049
霸天 羅 <braveteen@outlook.com>
2021-05-11 21:11:44 +0800

    Initial commit

---
+++ app/src/main/java/com/company1/kotlin_helloword/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.company1.kotlin_helloword
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
